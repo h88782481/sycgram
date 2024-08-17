c5b2a98f0e82d0e8ae3b8fd7972302e49105fef9
h88782481 <54714341+h88782481@users.noreply.github.com>
2024-08-17 23:14:33 +0800

    最后的更新

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
 from pyrogram.methods.utilities.idle import idle
-
-from core import app
+from core import user_bot
 from tools.initializer import init_logger
-from tools.sessions import session
 
 
 async def main():
     init_logger()
-    await app.start()
+    await user_bot.start()
     await idle()
-    await session.close()
-    await app.stop()
+    await user_bot.stop()
 
 
 if __name__ == '__main__':
-    app.run(main())
+    user_bot.run(main())
